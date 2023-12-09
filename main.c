c155c07cf490cbdb670e6f5a29f89f2a0a30284c
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2023-12-10 00:54:04 +0100

    [portability] more portable PI math. simple but effective, gathered ideas from stackoverflow.com

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 #include <math.h>
+#define math_pi (3.14159265358979323846264338327950288419716939937510)
 
 void logger(char *message)
 {
@@ -37,7 +38,7 @@ int main(int argc, char **argv)
         }
 
         // === OUTPUT ===
-        
+
         // clear to draw
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE); // white
         SDL_RenderClear(renderer);
@@ -45,8 +46,8 @@ int main(int argc, char **argv)
         // draw
         SDL_Rect rectangle = {.x = 50, .y = 100, .w = 500, .h = 200};
         Uint64 ticks = SDL_GetTicks64();
-        float pulse = fabs(sin(M_PI*ticks/1000));
-        SDL_SetRenderDrawColor(renderer, 255*pulse, 0, 0, SDL_ALPHA_OPAQUE);
+        float pulse = fabs(sin(math_pi * ticks / 1000));
+        SDL_SetRenderDrawColor(renderer, 255 * pulse, 0, 0, SDL_ALPHA_OPAQUE);
         SDL_RenderFillRect(renderer, &rectangle);
 
         // end draw
