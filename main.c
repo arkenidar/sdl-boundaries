95f2e13ab67d0a1c437da2cc8036a96d92307c35
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-12-10 19:14:11 +0100

    [tooling] main.c adapted to CLion

--- main.c
+++ main.c
@@ -3,36 +3,34 @@
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
 #include <math.h>
+
 #define math_pi (3.14159265358979323846264338327950288419716939937510)
 
-void logger(char *message)
-{
+void logger(char *message) {
     SDL_Log("%s", message);
 }
 
 // pointer for mouse input and/or touch-screen input
-struct pointer_struct
-{
+struct pointer_struct {
     int x, y;
     bool click, down, down_previously;
 } pointer;
-void pointer_init()
-{
+
+void pointer_init() {
     pointer.x = pointer.y = 0;
     pointer.click = pointer.down = pointer.down_previously = false;
 }
-void pointer_input()
-{
+
+void pointer_input() {
     Uint32 bitmask = SDL_GetMouseState(&pointer.x, &pointer.y);
     pointer.down_previously = pointer.down;
     pointer.down = bitmask & SDL_BUTTON_LMASK;
     pointer.click = pointer.down && !pointer.down_previously;
 }
 
-void draw1(SDL_Renderer *renderer, float pulse, SDL_Texture *texture1, bool shown)
-{
+void draw1(SDL_Renderer *renderer, double pulse, SDL_Texture *texture1, bool shown) {
     // draw: red pulsating rectangle
-    SDL_SetRenderDrawColor(renderer, 255 * pulse, 0, 0, SDL_ALPHA_OPAQUE); // red, pulsating
+    SDL_SetRenderDrawColor(renderer, (Uint8) (255 * pulse), 0, 0, SDL_ALPHA_OPAQUE); // red, pulsating
     SDL_Rect rectangle = {.x = 50, .y = 100, .w = 500, .h = 200};          // position and size of a rectangle
     SDL_RenderFillRect(renderer, &rectangle);                              // fill rectangle with color
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);     // white
@@ -48,12 +46,11 @@ void draw1(SDL_Renderer *renderer, float pulse, SDL_Texture *texture1, bool show
     SDL_Rect rectangle2 = {.x = pointer.x - size / 2, .y = pointer.y - size / 2, .w = size, .h = size};
     if (shown)
         SDL_RenderCopy(renderer, texture1, NULL, &rectangle2); // draw in rectangle2, if shown
-    SDL_ShowCursor(!shown);                                    // show default cursor image or custum cursor image
+    SDL_ShowCursor(!shown);                                    // show default cursor image or custom cursor image
 }
 
 // app entry-point "main()": execution of program begins here and terminates here
-int main(int argc, char **argv)
-{
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv) {
     // init program
     logger("this app is starting...");
     SDL_Init(SDL_INIT_VIDEO);
@@ -72,28 +69,25 @@ int main(int argc, char **argv)
 
     // run loop
     bool running = true;
-    while (running)
-    {
+    while (running) {
         // === INPUT ===
 
         pointer_input();
 
         // event handling loop
         SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            switch (event.type)
-            {
-            case SDL_QUIT:
-                running = false; // end the run loop
-                break;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = false; // end the run loop
+                    break;
             }
         }
 
         // === PROCESSING ===
 
         Uint64 ticks = SDL_GetTicks64();                 // elapsed time
-        float pulse = fabs(sin(math_pi * ticks / 1000)); // 0.0 <-> 1.0 pulsation in time
+        double pulse = fabs(sin(math_pi * ((double) ticks / 1000))); // 0.0 <-> 1.0 pulsation in time
 
         if (pointer.click)  // if clicked ...
             shown = !shown; // alternate: show/hide
