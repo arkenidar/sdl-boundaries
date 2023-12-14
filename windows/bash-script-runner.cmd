9be03cb978dbf6b464110257c7618da4e7cedb81
Dario Cangialosi <dario.cangialosi@gmail.com>
2023-12-14 11:58:35 +0100

    [MS Windows] runs a BASH script in Microsoft "cmd.exe" using "bash.exe" of Minimal GNU for Windows (MinGW of MSYS)

---
+++ windows/bash-script-runner.cmd
@@ -0,0 +1,4 @@
+rem [MS Windows] runs a BASH script in Microsoft "cmd.exe" using "bash.exe" of Minimal GNU for Windows (MinGW of MSYS)
+C:\msys64\usr\bin\bash.exe windows_setup.bash
+echo BASH exited
+pause
